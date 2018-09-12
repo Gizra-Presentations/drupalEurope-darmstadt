6917f06a06f265d4dde5333fd40333b487151dd4
Amitai Burstein <amitai@gizra.com>
2018-09-12 11:31:48 +0200

    More work

--- my-app/src/Main.elm
+++ my-app/src/Main.elm
@@ -61,11 +61,7 @@ viewImageAndCaption imageAndCaption =
         Image imageAttributes ->
             let
                 imgContent =
-                    img
-                        [ src imageAttributes.imageUrl
-                        , class "mw-100 dib measure f3"
-                        ]
-                        []
+                    img [ src imageAttributes.imageUrl ] []
 
                 captionContent =
                     case imageAttributes.caption of
@@ -83,8 +79,8 @@ viewImageAndCaption imageAndCaption =
 
 viewParagraphs : List String -> Html Msg
 viewParagraphs paragraphs =
-    div [ class "ph3 ph4-m ph5-l" ]
-        (List.map (\paragraph -> p [ class "f4 mb4 center measure lh-copy" ] [ text paragraph ]) paragraphs)
+    div []
+        (List.map (\paragraph -> p [] [ text paragraph ]) paragraphs)
 
 
 viewParagraphsCount : List String -> Html Msg
@@ -94,7 +90,7 @@ viewParagraphsCount paragraphs =
         count =
             List.foldl (\_ accum -> accum + 1) 0 paragraphs
     in
-    div [ class "ph3 ph4-m ph5-l" ]
+    div []
         [ text "Paragraphs Count: "
         , text <| String.fromInt count
         ]
@@ -102,83 +98,16 @@ viewParagraphsCount paragraphs =
 
 view : Model -> Html Msg
 view model =
-    article [ class "baskerville pb5" ]
-        [ header [ class "avenir tc-l ph3 ph4-ns pt4 pt5-ns" ]
+    article []
+        [ header []
             [ h1
-                [ class "f3 f2-m f-subheadline-l measure lh-title fw1 mt0" ]
+                []
                 [ text model.title ]
             ]
         , viewImageAndCaption model.imageAndCaption
         , viewParagraphs model.paragraphs
         , viewParagraphsCount model.paragraphs
         ]
-
-
-
---
---
--- article
---   [ class "baskerville pb5" ]
---   [ header
---     [ class "avenir tc-l ph3 ph4-ns pt4 pt5-ns" ]
---     [ h1
---       [ class "f3 f2-m f-subheadline-l measure lh-title fw1 mt0" ]
---       [ text "A Night of Taking Photos at the Spooky Ruins of the Sutro Baths" ]
---     , time
---       [ class "f5 f4-l db fw1 baskerville mb4" ]
---       [ text "March 19th, 2015" ]
---     , img
---       [ class "w-100 dib measure f3", src "http://mrmrs.github.io/photos/009.jpg", alt "A photo of sutro baths by Adam Morse" ]
---       []
---     ]
---   , div
---     [ class "ph3 ph4-m ph5-l" ]
---     [ p
---       [ class "f4 mb4 center measure lh-copy" ]
---       [ text "On"  time
---         []
---         [ text "March 14, 1896" ] text ", the Sutro Baths were opened to the public as the world's largest indoor swimming pool establishment."
---       ]
---     , div
---       [ class "measure db center f5 f4-ns lh-copy" ]
---       [ img
---         [ class "db w-100 mt4 mt5-ns", src "http://mrmrs.github.io/photos/013.jpg", alt "Photo of the foundations of a building on a cliff overlooking a lighthouse." ]
---         []
---       , p
---         []
---         [ text "Before it burned to the ground, the structure filled a small beach
---         inlet below the Cliff House, also owned by Adolph Sutro at the time.
---         Shortly after closing, a fire in 1966 destroyed the building
---         while it was in the process of being demolished." ]
---       , img
---         [ class "db w-100 mt4 mt5-ns", src "http://mrmrs.github.io/photos/012.jpg", alt "Photo of the sea with mist covering the rocky formations near the shore." ]
---         []
---       , p
---         []
---         [ text "During high tides, water would flow directly into the pools from the
---         nearby ocean, recycling the two million US gallons of water in about an hour." ]
---       , img
---         [ class "db w-100 mt4 mt5-ns", src "http://mrmrs.github.io/photos/010.jpg", alt "Photo of a dusk skyline above a grassy rockface covered in trees." ]
---         []
---       , p
---         []
---         [ text "All that remains
---         of the site are concrete walls, blocked off stairs and
---         passageways, and a tunnel with a deep crevice in the middle. The
---         cause of the fire was arson. Shortly afterwards, the developer
---         left San Francisco and claimed insurance money." ]
---       , img
---         [ class "db w-100 mt4 mt5-ns", src "http://mrmrs.github.io/photos/011.jpg", alt "Photo of the sea and sky on the horizon with the foundations of a demolished house." ]
---         []
---       , p
---         []
---         [ text "During low tides, a powerful turbine water pump,
---         built inside a cave at sea level, could be switched on from a control
---         room and could fill the tanks at a rate of 6,000 US gallons a minute,
---         recycling all the water in five hours." ]
---       ]
---     ]
---   ]
 
 
 main : Program () Model Msg
